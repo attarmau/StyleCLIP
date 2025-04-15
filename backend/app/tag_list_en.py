5d7946000be4f752e3ed70291ae659a41071ecf5
attarmau <attarmau111@gmail.com>
2025-04-15 11:47:38 +0800

    Create tag_list_en.py

---
+++ backend/app/tag_list_en.py
@@ -0,0 +1,11 @@
+tag_dict = {
+    "General Features": ["Plain", "Patterned", "Color Block", "Embroidered"],
+    "Colors": ["Black", "White", "Blue", "Red", "Green", "Beige", "Pink", "Yellow", "Brown", "Gray"],
+    "Fabrics": ["Cotton", "Denim", "Silk", "Lace", "Knitted", "Linen", "Wool", "Satin", "Leather"],
+    "Fit": ["Loose", "Slim", "Oversized", "Regular Fit"],
+    "Length": ["Cropped", "Hip Length", "Mid Length", "Long"],
+    "Style": ["Casual", "Formal", "Sporty", "Chic", "Sweet", "Vintage", "Street", "Minimalist"],
+    "Occasion": ["Daily", "Work", "Party", "Vacation", "Date", "Outdoor", "Homewear"],
+    "Clothing Type": ["T-shirt", "Shirt", "Blouse", "Dress", "Jacket", "Coat", "Sweater", "Hoodie", "Skirt", "Pants", "Jeans"],
+    "Details": ["Zipper", "Buttons", "Ruffles", "Hood", "Drawstring", "Pockets", "Belted", "Pleated"]
+}
