4b069f389e68bb1327b76304072c692bd65b1ee6
attarmau <attarmau111@gmail.com>
2025-04-25 00:10:54 +0800

    Update database.py

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,16 +1,20 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from backend.app.config.settings import settings
+from typing import AsyncGenerator
 
-client = None
-db = None
+client: AsyncIOMotorClient = None
+db: AsyncIOMotorDatabase = None
 
 async def init_db():
     global client, db
     client = AsyncIOMotorClient(settings.MONGO_URI)
-    db = client[settings.MONGO_DB_NAME]  # MongoDB database name from settings
+    db = client[settings.MONGO_DB_NAME]
     print("Database connected")
 
 async def close_db():
     global client
     client.close()
     print("Database connection closed")
+
+async def get_db() -> AsyncGenerator[AsyncIOMotorDatabase, None]:
+    yield db
