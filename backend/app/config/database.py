5a25f4a68760d8c3a8957171a05e45dd82691147
attarmau <attarmau111@gmail.com>
2025-04-24 13:17:45 +0800

    Update database.py

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,1 +1,18 @@
+# /backend/app/config/database.py
 
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.config.settings import settings
+
+client = None
+db = None
+
+async def init_db():
+    global client, db
+    client = AsyncIOMotorClient(settings.MONGO_URI)  # Set the Mongo URI from settings
+    db = client[settings.MONGO_DB_NAME]  # Name of your database
+    print("Database connected")
+
+async def close_db():
+    global client
+    client.close()
+    print("Database connection closed")
