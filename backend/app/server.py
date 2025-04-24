4566283711b4e3515ee01f5e52dbe6231e73f859
attarmau <attarmau111@gmail.com>
2025-04-24 23:50:50 +0800

    Update server.py

--- backend/app/server.py
+++ backend/app/server.py
@@ -25,7 +25,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://yourfrontenddomain.com"],  # Update this as needed
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
