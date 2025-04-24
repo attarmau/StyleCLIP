0375ab45573d9bef1bb20610e0b60710fca0ccd5
attarmau <attarmau111@gmail.com>
2025-04-24 21:48:33 +0800

    Update tag_list_en.py

--- backend/app/config/tag_list_en.py
+++ backend/app/config/tag_list_en.py
@@ -1,11 +1,97 @@
-tag_dict = {
-    "General Features": ["Plain", "Patterned", "Color Block", "Embroidered"],
-    "Colors": ["Black", "White", "Blue", "Red", "Green", "Beige", "Pink", "Yellow", "Brown", "Gray"],
-    "Fabrics": ["Cotton", "Denim", "Silk", "Lace", "Knitted", "Linen", "Wool", "Satin", "Leather"],
-    "Fit": ["Loose", "Slim", "Oversized", "Regular Fit"],
-    "Length": ["Cropped", "Hip Length", "Mid Length", "Long"],
-    "Style": ["Casual", "Formal", "Sporty", "Chic", "Sweet", "Vintage", "Street", "Minimalist"],
-    "Occasion": ["Daily", "Work", "Party", "Vacation", "Date", "Outdoor", "Homewear"],
-    "Clothing Type": ["T-shirt", "Shirt", "Blouse", "Dress", "Jacket", "Coat", "Sweater", "Hoodie", "Skirt", "Pants", "Jeans"],
-    "Details": ["Zipper", "Buttons", "Ruffles", "Hood", "Drawstring", "Pockets", "Belted", "Pleated"]
+# /backend/app/config/tag_list_en.py
+
+GARMENT_TYPES = {
+    "Tops": {
+        "fabric": [
+            "Dri-FIT", "Tulle", "Suede", "Canvas", "Faux fur", "Fleece", "Brocade", "Neoprene", "Waterproof fabric",
+            "Windbreaker fabric", "Thermal fabric", "Activewear fabric", "Metallic fabric", "Knit", "Crochet knit",
+            "Embellished fabric", "Eyelet fabric", "Ripstop fabric", "Textured fabric", "Seersucker", "Embroidered Mesh",
+            "Ribbed Knit"
+        ],
+        "silhouette": [
+            "Straight silhouette", "Slim fit", "Natural fit", "Flare", "Trapeze", "Fitted", "Fit-and-flare", "Boxy",
+            "High-waisted silhouette"
+        ],
+        "fit": [
+            "Tight fit", "Slim fit", "Regular fit", "Relaxed fit", "Loose fit", "Oversized fit", "Tailored fit",
+            "Softly tailored fit", "Structured fit"
+        ],
+        "color": [
+            "White", "Ivory", "Off-white", "Bone", "Black", "Black melange", "Charcoal", "Grey", "Heather gray", "Light grey",
+            "Graphite grey", "Gunmetal"
+        ]
+    },
+    "Pants": {
+        "fabric": [
+            "Denim", "Cotton", "Polyester", "Linen", "Corduroy"
+        ],
+        "silhouette": [
+            "Straight", "Slim fit", "Baggy", "Peg top", "Boot cut", "Bell bottom", "Flare", "Wide leg", "Skinny"
+        ],
+        "fit": [
+            "Tight fit", "Slim fit", "Relaxed fit", "Loose fit", "Regular fit"
+        ],
+        "color": [
+            "Navy", "Black", "Beige", "Khaki", "Gray", "Charcoal", "Olive"
+        ]
+    },
+    "Dress": {
+        "fabric": [
+            "Cotton", "Linen", "Silk", "Velvet", "Jersey", "Satin", "Chiffon", "Organza"
+        ],
+        "silhouette": [
+            "A-line", "H-line / Straight shape", "I-line / Column / Shift", "V-line", "X-line", "Y-line", "Tent shape",
+            "Natural shape", "Sheath", "Fit-and-flare", "Slim", "Hourglass", "Asymmetric", "Princess line", "Empire line",
+            "Balloon", "Shift"
+        ],
+        "fit": [
+            "Tight fit", "Slim fit", "Regular fit", "Relaxed fit", "Loose fit", "Oversized fit", "Tailored fit"
+        ],
+        "color": [
+            "Red", "Pink", "Purple", "Blue", "Green", "Yellow", "Orange", "Black", "White", "Ivory"
+        ]
+    },
+    "One-piece": {
+        "fabric": [
+            "Neoprene", "Spandex", "Jersey", "Polyester", "Lycra"
+        ],
+        "silhouette": [
+            "Straight-cut", "Wide-leg", "Flare", "Bell bottom", "Tapered", "Slim-fit", "Skinny"
+        ],
+        "fit": [
+            "Tight fit", "Slim fit", "Relaxed fit"
+        ],
+        "color": [
+            "Black", "White", "Navy", "Red", "Green"
+        ]
+    },
+    "Skirts": {
+        "fabric": [
+            "Denim", "Cotton", "Leather", "Silk", "Velvet", "Linen", "Jersey"
+        ],
+        "silhouette": [
+            "A-line skirts", "Circle skirts", "Straight skirt", "Pencil skirts", "Gored skirt", "Bias-cut skirt", "Flared skirt",
+            "Fitted skirt", "Asymmetrical skirt", "Draped skirt", "Trumpet", "Mermaid"
+        ],
+        "fit": [
+            "Tight fit", "Relaxed fit", "Regular fit", "Loose fit"
+        ],
+        "color": [
+            "Black", "White", "Red", "Navy", "Beige", "Grey", "Brown", "Green"
+        ]
+    },
+    "Outerwear": {
+        "fabric": [
+            "Wool", "Leather", "Polyester", "Denim", "Faux fur", "Corduroy", "Nylon", "Cashmere"
+        ],
+        "silhouette": [
+            "Straight-cut", "A-line", "Slim fit", "Natural fit", "Flare", "Fitted", "Fit-and-flare", "Cocoon"
+        ],
+        "fit": [
+            "Tight fit", "Relaxed fit", "Oversized fit", "Tailored fit", "Regular fit"
+        ],
+        "color": [
+            "Black", "Beige", "Navy", "Grey", "Red", "Green", "Camel", "Charcoal"
+        ]
+    }
 }
